dbd8c1f8e0bf9a0bc7ca47b6c910962e1cabd4a6
Muhammad Abdulloh <clevercoderr@gmail.com>
2024-02-19 12:41:52 +0500

    linq lesson 1 finished

--- LINQLess/Program.cs
+++ LINQLess/Program.cs
@@ -10,35 +10,20 @@ namespace LINQLess
         {
 
             LINQForEduCenter data = new LINQForEduCenter();
-            //foreach (var item in data.GetCenterByName("Najot Ta'lim"))
-            //{
-            //    var empoyees = item.Employees.Select(x => x).Where(z => z.Experience > 5);
-            //    Console.WriteLine(item.Name);
-            //    foreach(var employee in empoyees)
-            //    {
-            //        Console.WriteLine(employee.FirstName + " " + employee.LastName);
-            //    }
-            //}
-
-            //foreach(var item in data.GetCenterByNameWithExperience())
-            //{
-            //    Console.WriteLine(item.FirstName + " " + item.Experience);
-            //}
-
+            
 
             List<int> numbers = new List<int>()
             {
-                1, 2, 3, 4, 5, 657,45 ,567,5,75,7,5,8,68,6, 0,78,6,6,9,9 ,3412 ,978 ,9,789,2342,637,213
+                1, 2, 2, 3, 3, 4, 5, 6
             };
 
             List<int> numbers2 = new List<int>()
             {
-                2, 1, 3, 4, 5, 657,45 ,567,5,75,7,5,8,68,6, 0,78,6,6,9,9 ,3412 ,978 ,9,789,2342,637,213
+                5, 6, 7, 8, 8, 9, 1
             };
 
-            var result = numbers.SequenceEqual(numbers2);
 
-            Console.WriteLine(result);
+            Console.WriteLine(numbers2.Average());
         }
     }
 }
